f67c7db9e9e9776a814ade45405b12406b65d0b9
Ken Bergers <SWP679@mon.local>
2018-06-14 16:58:58 +0200

    Added spatial_queries.py

--- intro-to-python-gis/spatial_queries.py
+++ intro-to-python-gis/spatial_queries.py
@@ -1,7 +1,23 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Jun 14 16:28:08 2018
+import pandas as pd
+import geopandas as gpd
+from shapely.geometry import Point
+import geocoder
+from fiona.crs import from_epsg
 
-@author: SWP679
-"""
+fp = r"shopping_centers.txt"
+data = pd.read_csv(fp, sep=';')
 
+#Get lat and lon based on adresses using OSM as provider
+geo_latlng = [geocoder.osm(adress).latlng for adress in data["addres"]]
+#Convert to Point geometry
+geo_point = [Point(addr[0], addr[1]) for addr in geo_latlng]
+
+#Add points to new column "geometry" in GeoDataFrame
+geo = gpd.GeoDataFrame(data)
+geo["geometry"] = geo_point
+geo.crs = from_epsg(3879)
+
+#Output to shapefile
+fp = "C:\\temp\\shp\\helsinkin_shopping_centers.shp"
+geo.to_file(fp)
